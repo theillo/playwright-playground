7c9527b156cbd673aaaad5772ec1a890e7e83aa6
mateusz.czerewko@wearecogworks.com <mateusz.czerewko@wearecogworks.com>
2023-06-28 14:43:12 +0200

    new desktop tests (tricky popup)

---
+++ README.md
@@ -0,0 +1,2 @@
+## Read medsa 
+- sdadas
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,9 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("login with correct credentials", async ({ page }) => {
   await page.goto("https://demo-bank.vercel.app/");
-  await page.getByTestId("login-input").click();
   await page.getByTestId("login-input").fill("test1234");
-  await page.getByTestId("password-input").click();
   await page.getByTestId("password-input").fill("test1234");
   await page.getByTestId("login-button").click();
   await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
@@ -12,18 +10,18 @@ test("login with correct credentials", async ({ page }) => {
 
 test("login with incorrect username", async ({ page }) => {
   await page.goto("https://demo-bank.vercel.app/");
-  await page.getByTestId("login-input").click();
+  // await page.getByTestId("login-input").click();
   await page.getByTestId("login-input").fill("test");
   await page.getByTestId("password-input").click();
   await expect(page.getByTestId("error-login-id")).toHaveText(
     "identyfikator ma min. 8 znaków"
   );
 });
-test.only("login with incorrect password", async ({ page }) => {
+test("login with incorrect password", async ({ page }) => {
   await page.goto("https://demo-bank.vercel.app/");
-  await page.getByTestId("login-input").click();
+  // await page.getByTestId("login-input").click();
   await page.getByTestId("login-input").fill("123testz");
-  await page.getByTestId("password-input").click();
+  // await page.getByTestId("password-input").click();
   await page.getByTestId("password-input").fill("32");
   // await page.locator('#login_password_container label').click();
   await page.getByTestId("password-input").blur();
