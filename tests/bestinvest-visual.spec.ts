9c8d4067b2a41820a51f82eeac2b7fa7f088aa40
mateusz.czerewko@wearecogworks.com <mateusz.czerewko@wearecogworks.com>
2023-07-03 10:31:28 +0200

    new test for adding cash for mobile

--- tests/bestinvest-visual.spec.ts
+++ tests/bestinvest-visual.spec.ts
@@ -24,8 +24,8 @@ test.describe('Full screen page', () => {
   });
 });
 
-test.describe('Iframe check', () => {
-  test.only('Iframe check', async ({ page }) => {
+test.describe('Img check', () => {
+  test('Img check', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
     await page.goto('http://localhost:3000/pension-calculator/');
     const locatorX = page.locator('#testVisual')
@@ -38,3 +38,15 @@ test.describe('Iframe check', () => {
 
 
 // npx playwright test --update-snapshots
+
+// maxDiffPixelRatio - this value can be between 0 and 1 and is defined as the acceptable amount of pixels that can differ from the total amount of pixels.
+// maxDiffPixels - this can be any value and is just a count of how many pixels can be different - it's worth experimenting with your test execution and seeing what an acceptable difference is.
+// Threshold - this value can be between 0 (strict) and 1 (lax) and is the acceptable perceived color difference between the same pixel in the compared images - again, this is worth experimenting with and seeing how strict you want it to be.
+// await expect(headerLogo).toHaveScreenshot({ maxDiffPixelRatio: 0.1 })
+// await expect(headerLogo).toHaveScreenshot({ maxDiffPixels: 100 })
+// await expect(headerLogo).toHaveScreenshot({ threshold: 0.1 })
+
+// for full page 
+// animations: “disabled” - this will stop any CSS animations or transitions on your webpage.
+// maxDiffPixelRatio: 0.2 - which we covered earlier, will allow some room for minor differences.
+// await expect(page).toHaveScreenshot({ fullPage: true, animations: "disabled", maxDiffPixelRatio: 0.2 });
--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 
-
+//to invastingationß
 test.describe("Desktop tests", () => {
     test('quick payment with correct data', async ({ page }) => {
         await page.goto('https://demo-bank.vercel.app/');
@@ -12,9 +12,31 @@ test.describe("Desktop tests", () => {
         await page.locator('#widget_1_transfer_amount').fill('150');
         await page.locator('#widget_1_transfer_title').fill('pizza');
         await page.getByRole('button', { name: 'wykonaj' }).click();
+        const popupClose = '.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons';
+        // await page.waitForSelector(popupClose);
+        await expect(page.locator(popupClose)).toHaveCSS('display', 'block')
         await page.getByTestId('close-button').click();
-        await expect(page.locator('#show_messages')).toHaveText(
-          'Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza'
-        );
+        // await page.waitForTimeout(1);
+        // await expect(page.locator('#show_messages')).toHaveText(
+        //   'Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza'
+        // );
+      });
+      test('sucesfull mobile top-up', async ({ page }) => {
+        await page.goto('https://demo-bank.vercel.app/');
+        await page.getByTestId('login-input').fill('test1231');
+        await page.getByTestId('password-input').fill('test1231');
+        await page.getByTestId('login-button').click();
+        // await page.selectOption('#widget_1_topup_receiver', '502 xxx xxx');
+        await page.locator('#widget_1_topup_receiver').selectOption('504 xxx xxx');
+        await page.locator('#widget_1_topup_amount').fill('40');
+        // await page.locator('#uniform-widget_1_topup_agreement span').click();
+        await page.locator('#uniform-widget_1_topup_agreement span').check();
+        // await page.getByRole('button', { name: 'doładuj telefon' }).click();
+        await page.locator('#execute_phone_btn').click();
+        await expect(page.locator('.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons')).toHaveCSS('display', 'block');
+        await expect(page.locator('#show_messages')).toHaveText('Doładowanie wykonane! 40,00PLN na numer 504 xxx xxx');
+        await page.getByTestId('close-button').click();
+       
+        
       });
 });
