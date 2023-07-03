6b533a5c3a16f4f13b1ff1afec6e59c97455900f
mateusz.czerewko@wearecogworks.com <mateusz.czerewko@wearecogworks.com>
2023-07-03 15:15:28 +0200

    refactoring

--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -4,37 +4,51 @@ import { test, expect } from "@playwright/test";
 //to invastingationß
 test.describe("Desktop tests", () => {
     test('quick payment with correct data', async ({ page }) => {
-        await page.goto('https://demo-bank.vercel.app/');
-        await page.getByTestId('login-input').fill('testerLO');
-        await page.getByTestId('password-input').fill('password');
+      //Arrange
+      const url = 'https://demo-bank.vercel.app/';
+      const userId = 'testerLO';
+      const newPassword = 'password';
+      const receiverID = '2';
+      const amoutTransfer = '150';
+      const transferTitle = 'pizza';
+      const popupClose = '.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons';
+      //Act
+        await page.goto(url);
+        await page.getByTestId('login-input').fill(userId);
+        await page.getByTestId('password-input').fill(newPassword);
         await page.getByTestId('login-button').click();
-        await page.locator('#widget_1_transfer_receiver').selectOption('2');
-        await page.locator('#widget_1_transfer_amount').fill('150');
-        await page.locator('#widget_1_transfer_title').fill('pizza');
+        await page.locator('#widget_1_transfer_receiver').selectOption(receiverID);
+        await page.locator('#widget_1_transfer_amount').fill(amoutTransfer);
+        await page.locator('#widget_1_transfer_title').fill(transferTitle);
         await page.getByRole('button', { name: 'wykonaj' }).click();
-        const popupClose = '.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons';
-        // await page.waitForSelector(popupClose);
-        await expect(page.locator(popupClose)).toHaveCSS('display', 'block')
+        await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza');
+        await expect(page.locator(popupClose)).toHaveCSS('display', 'block');
         await page.getByTestId('close-button').click();
         // await page.waitForTimeout(1);
-        // await expect(page.locator('#show_messages')).toHaveText(
-        //   'Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza'
-        // );
       });
-      test('sucesfull mobile top-up', async ({ page }) => {
-        await page.goto('https://demo-bank.vercel.app/');
-        await page.getByTestId('login-input').fill('test1231');
-        await page.getByTestId('password-input').fill('test1231');
+      test.only('sucesfull mobile top-up', async ({ page }) => {
+        //Arrange
+        const url = 'https://demo-bank.vercel.app/';
+        const userID = 'test1231';
+        const userPassword = 'test1231';
+        const phoneNumber = '504 xxx xxx';
+        let amountAdded = '40,21';
+        if (!/\.\d{2}$/.test(amountAdded)) {
+          amountAdded += ',00';
+        }
+        const expectedMessage = `Doładowanie wykonane! ${amountAdded}PLN na numer ${phoneNumber}`;
+        //Act
+        await page.goto(url);
+        await page.getByTestId('login-input').fill(userID);
+        await page.getByTestId('password-input').fill(userPassword);
         await page.getByTestId('login-button').click();
-        // await page.selectOption('#widget_1_topup_receiver', '502 xxx xxx');
-        await page.locator('#widget_1_topup_receiver').selectOption('504 xxx xxx');
-        await page.locator('#widget_1_topup_amount').fill('40');
-        // await page.locator('#uniform-widget_1_topup_agreement span').click();
+        await page.locator('#widget_1_topup_receiver').selectOption(phoneNumber);
+        await page.locator('#widget_1_topup_amount').fill(amountAdded);
         await page.locator('#uniform-widget_1_topup_agreement span').check();
-        // await page.getByRole('button', { name: 'doładuj telefon' }).click();
         await page.locator('#execute_phone_btn').click();
+        //Assert
         await expect(page.locator('.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons')).toHaveCSS('display', 'block');
-        await expect(page.locator('#show_messages')).toHaveText('Doładowanie wykonane! 40,00PLN na numer 504 xxx xxx');
+        await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
         await page.getByTestId('close-button').click();
       });
 });
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,18 @@
 import { test, expect } from "@playwright/test";
 
 test("login with correct credentials", async ({ page }) => {
-  await page.goto("https://demo-bank.vercel.app/");
-  await page.getByTestId("login-input").fill("test1234");
-  await page.getByTestId("password-input").fill("test1234");
+  //Arrange
+  const url = "https://demo-bank.vercel.app/";
+  const userId = "test1234";
+  const userPassword = "test1234";
+  const expectedUserName = "Jan Demobankowy";
+  //Act
+  await page.goto(url);
+  await page.getByTestId("login-input").fill(userId);
+  await page.getByTestId("password-input").fill(userPassword);
   await page.getByTestId("login-button").click();
-  await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
+  //Assert
+  await expect(page.getByTestId("user-name")).toHaveText(expectedUserName);
 });
 
 test("login with incorrect username", async ({ page }) => {
@@ -29,3 +36,6 @@ test("login with incorrect password", async ({ page }) => {
     "hasło ma min. 8 znaków"
   );
 });
+
+// ctrl + shift + R - zrobienie refaktoryzacji
+// alt + strzalka przesuniecie lini
