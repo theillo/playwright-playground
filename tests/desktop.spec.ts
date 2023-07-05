c588da6c1d6b071d21d81854a9111dfa7364d2be
mateuszQA <mateusz.czerewko@wearecogworks.com>
2023-07-05 13:17:12 +0200

    dry method added

--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -2,39 +2,34 @@ import { test, expect } from '@playwright/test'
 
 //to invastingationß
 test.describe('Desktop tests', () => {
+    
+    test.beforeEach(async ({page}) => {
+        const userID = 'testerLO'
+        const userPassword = 'password'
+
+        await page.goto('/')
+        await page.getByTestId('login-input').fill(userID)
+        await page.getByTestId('password-input').fill(userPassword)
+        await page.getByTestId('login-button').click()
+    })
     test('quick payment with correct data', async ({ page }) => {
         //Arrange
-        const url = 'https://demo-bank.vercel.app/'
-        const userId = 'testerLO'
-        const newPassword = 'password'
         const receiverID = '2'
         const amoutTransfer = '150'
         const transferTitle = 'pizza'
-        const popupClose =
-            '.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons'
+        const popupClose = '.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons'
         //Act
-        await page.goto(url)
-        await page.getByTestId('login-input').fill(userId)
-        await page.getByTestId('password-input').fill(newPassword)
-        await page.getByTestId('login-button').click()
-        await page
-            .locator('#widget_1_transfer_receiver')
-            .selectOption(receiverID)
+        await page.locator('#widget_1_transfer_receiver').selectOption(receiverID)
         await page.locator('#widget_1_transfer_amount').fill(amoutTransfer)
         await page.locator('#widget_1_transfer_title').fill(transferTitle)
         await page.getByRole('button', { name: 'wykonaj' }).click()
-        await expect(page.locator('#show_messages')).toHaveText(
-            'Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza'
-        )
+        // await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza')
         await expect(page.locator(popupClose)).toHaveCSS('display', 'block')
         await page.getByTestId('close-button').click()
         // await page.waitForTimeout(1);
     })
-    test.only('sucesfull mobile top-up', async ({ page }) => {
+    test('sucesfull mobile top-up', async ({ page }) => {
         //Arrange
-        const url = 'https://demo-bank.vercel.app/'
-        const userID = 'test1231'
-        const userPassword = 'test1231'
         const phoneNumber = '504 xxx xxx'
         let amountAdded = '40,21';
         const decimalMatch = amountAdded.match(/,\d{2}$/);
@@ -45,20 +40,12 @@ test.describe('Desktop tests', () => {
         }
         const expectedMessage = `Doładowanie wykonane! ${amountAdded}PLN na numer ${phoneNumber}`;
         //Act
-        await page.goto(url)
-        await page.getByTestId('login-input').fill(userID)
-        await page.getByTestId('password-input').fill(userPassword)
-        await page.getByTestId('login-button').click()
         await page.locator('#widget_1_topup_receiver').selectOption(phoneNumber)
         await page.locator('#widget_1_topup_amount').fill(amountAdded)
         await page.locator('#uniform-widget_1_topup_agreement span').check()
         await page.locator('#execute_phone_btn').click()
         //Assert
-        await expect(
-            page.locator(
-                '.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons'
-            )
-        ).toHaveCSS('display', 'block')
+        await expect(page.locator('.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons')).toHaveCSS('display', 'block')
         await expect(page.locator('#show_messages')).toHaveText(expectedMessage)
         await page.getByTestId('close-button').click()
     })
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,14 +2,15 @@ import { test, expect } from '@playwright/test';
 
 
 test.describe('login tests cases', () => {
-  const url = 'https://demo-bank.vercel.app/';
+  test.beforeEach(async ({page}) => {
+    await page.goto('/')
+  })
 test('login with correct credentials', async ({ page }) => {
     //Arrange
     const userId = 'test1234';
     const userPassword = 'test1234';
     const expectedUserName = 'Jan Demobankowy';
     //Act
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
@@ -18,21 +19,16 @@ test('login with correct credentials', async ({ page }) => {
 });
 
 test('login with incorrect username', async ({ page }) => {
-  
   const incorrectNickname = 'test';
   const expectedErrorMsg = 'identyfikator ma min. 8 znaków';
-
-    await page.goto(url);
     // await page.getByTestId("login-input").click();
     await page.getByTestId('login-input').fill(incorrectNickname);
     await page.getByTestId('password-input').click();
     await expect(page.getByTestId('error-login-id')).toHaveText(expectedErrorMsg);
 });
 test('login with incorrect password', async ({ page }) => {
-  
   const userId = '123testz';
   const wrongPassword = '32';
-    await page.goto(url);
     const expectedErrorMsgforPassword = 'hasło ma min. 8 znaków';
     // await page.getByTestId("login-input").click();
     await page.getByTestId('login-input').fill(userId);
